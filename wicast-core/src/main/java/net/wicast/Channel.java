ceadcc4dbe3b83557642611c9d7334e5240c3820
Martin Spamer <martin.spamer@gmail.com>
2018-06-30 23:38:52 +0100

    Message & Channels

--- wicast-core/src/main/java/net/wicast/Channel.java
+++ wicast-core/src/main/java/net/wicast/Channel.java
@@ -1,5 +1,11 @@
+
 package net.wicast;
 
 public class Channel {
+
+    public Channel send(final Message message) {
+        // TODO Auto-generated method stub
+        return this;
+    }
 
 }
--- wicast-core/src/main/java/net/wicast/Message.java
+++ wicast-core/src/main/java/net/wicast/Message.java
@@ -4,6 +4,7 @@ package net.wicast;
 public class Message {
 
     public Message send(final Channel channel) {
+        // TODO Auto-generated method stub
         return this;
     }
 
